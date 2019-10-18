96705f5618fe679b86e54c66f51eb0046d1e1a54
Levon <levon.python@gmail.com>
2019-10-19 02:18:14 +0400

    New web-site scrap added - job_fincder.am

--- job_searching.py
+++ job_searching.py
@@ -3,25 +3,33 @@ import requests
 import csv
 
 
-class JobsScrapping:
-    __slots__ = "staff_url", "hr_url", 'data_csv', 'keyword'
+class Exceptions(Exception):
+    pass
 
-    def __init__(self, staff_url, hr_url, data_csv, keyword):
+
+class JobsScrapping:
+    __slots__ = "staff_url", "jobdinfer_url", "hr_url", 'data_csv', 'keyword'
+
+    def __init__(self, staff_url, jobdinfer_url, hr_url, data_csv, keyword):
         self.staff_url = staff_url
+        self.jobdinfer_url = jobdinfer_url
         self.hr_url = hr_url
         self.data_csv = data_csv
         self.keyword = keyword
 
     def csv_file_open(self):
         with open(self.data_csv, 'w', newline='', encoding="utf-8") as csv_file:
-            csv_writer = csv.DictWriter(csv_file, fieldnames=['title', 'job_title_eng', 'deadline', 'employment_term',
-                                                              'job_type', 'category', 'location', 'job_link'])
+            csv_writer = csv.DictWriter(csv_file, fieldnames=[
+                'title', 'job_title_eng', 'company_name', 'deadline', 'employment_term', 'job_type',
+                'category', 'location', 'job_link'])
             csv_writer.writeheader()
             self.staff_am_scrap(csv_writer, 15)
-            self.hr_am_scrap(csv_writer, 32)
+            # self.hr_am_scrap(csv_writer, 32)
+            self.jobfinder_am_scrap(csv_writer)
 
+    # ----------------------------- S T A F F . A M -----------------------------------------------------------
     def staff_am_scrap(self, csv_writer, step=50,
-                       title='', deadline='', employment_term='',
+                       title='', company_name='', deadline='', employment_term='',
                        job_type='', category='', location=''):
 
         for page in range(0, step):
@@ -74,23 +82,106 @@ class JobsScrapping:
                                     location = elem_scd[5].strip()
                             jobs_info = [{'title': title,
                                           'job_title_eng': job_title_eng,
+                                          'company_name': company_name,
                                           'deadline': deadline,
                                           'employment_term': employment_term,
                                           'job_type': job_type,
                                           'category': category,
                                           'location': location,
-                                          'job_link': job_link
+                                          'job_link': job_link,
                                           }]
                             for jobs in jobs_info:
                                 csv_writer.writerow(jobs)
                     except Exception as err:
-                        print(err)
+                        Exceptions(f"ERROR {err}")
                         continue
 
+    # ----------------------------- J O B F I N D E R  . A M ---------------------------------------------------------
+    def jobfinder_am_scrap(self, csv_writer, job_title_eng=''):
+        source = requests.get(self.jobdinfer_url).text
+        soup = BeautifulSoup(source, "lxml")
+        job_list = soup.find('table', class_="grid")
+
+        for job in job_list.select('tr'):  # CSS selector
+
+            find_title = job.select_one('td:nth-last-child(3)')
+            try:
+                title = find_title.a.text.strip()
+            except Exception as err:
+                Exceptions(f"ERROR {err}")
+                continue
+            if self.keyword in title.lower():
+                company = job.select_one('td:nth-last-child(2)')
+                try:
+                    company_name = company.span.text
+                except Exception as err:
+                    Exceptions(f"ERROR {err}")
+                    continue
+
+                find_deadline = job.select_one('td:nth-child(3)')
+                try:
+                    deadline = find_deadline.text.strip()
+                except Exception as err:
+                    Exceptions(f"ERROR {err}")
+                    continue
+
+                find_job_link = job.select_one('td:nth-last-child(3)')
+                try:
+                    job_link = 'http://jobfinder.am/{}'.format(
+                        find_job_link.find('a', title="Display in new window")['href'])
+
+                    # opening each job description
+                    open_job_link = requests.get(job_link).text
+                    job_soup = BeautifulSoup(open_job_link, 'lxml')
+                    table = job_soup.find('table', class_="fieldtable")
+                    location = table.find(
+                        'span', id="ctl00_bdyPlaceHolde_jfpanelViewJob_jfJobPreview_lblLocation").text
+                    category = table.find(
+                        'span', id="ctl00_bdyPlaceHolde_jfpanelViewJob_jfJobPreview_lblCategory").text
+                    job_type = table.find(
+                        'span', id="ctl00_bdyPlaceHolde_jfpanelViewJob_jfJobPreview_lblPositionType").text
+                    if job_type in ("Full Time", "Լրիվ դրույք", "Полная ставка"):
+                        employment_term = "Permanent"
+                    elif job_type in "Internship":
+                        employment_term = "Part time"
+                    elif job_type in "Casual":
+                        employment_term = "Part time"
+                    elif job_type in "Պրակտիկա":
+                        employment_term = "Permanent"
+                    else:
+                        employment_term = "Other"
+
+                    jobs_info = [{'title': title,
+                                  'job_title_eng': job_title_eng,
+                                  'company_name': company_name,
+                                  'deadline': deadline,
+                                  'employment_term': employment_term,
+                                  'job_type': job_type,
+                                  'category': category,
+                                  'location': location,
+                                  'job_link': job_link,
+                                  }]
+                    for jobs in jobs_info:
+                        csv_writer.writerow(jobs)
+
+                    # # if content includes a keyword not in position but in Required qualifications
+                    # req_qual = job_soup.find(
+                    #     'span', id="ctl00_bdyPlaceHolde_jfpanelViewJob_jfJobPreview_lblRequiredQualifications").text
+                    # if self.keyword in req_qual.lower() or self.keyword in title.lower():
+                    #     for jobs in jobs_info:
+                    #         csv_writer.writerow(jobs)
+
+                except Exception as err:
+                    Exceptions(f"ERROR {err}")
+                    continue
+
+    # ----------------------------- H R . A M -----------------------------------------------------------
     def hr_am_scrap(self, csv_writer, step=50):
-        source = requests.get(self.hr_url).text
-        soup = BeautifulSoup(source, 'lxml')
-        # job = soup.find('div', class_="logo")
+        pass
+        # source = requests.get(self.hr_url).text
+        # soup = BeautifulSoup(source, 'html.parser')
+        # print(soup.prettify())
+        # job = soup.find('div', class_="vacancy-item")
         # js , can't open the job content
         # opens = requests.get(job)
         # so = BeautifulSoup(opens, 'lxml')
@@ -101,6 +192,7 @@ class JobsScrapping:
 
 if __name__ == "__main__":
     Staff_url = "https://staff.am/en/jobs?page={}&per-page=50"
+    Jobdinfer_url = "http://jobfinder.am/default.aspx"
     Hr_url = "http://hr.am/"
     # Keyword = 'intern'
     # Keyword = 'accountant'
@@ -108,5 +200,5 @@ if __name__ == "__main__":
     # Keyword = 'administrator'
     # Keyword = input("Please fill position: ")
     Data_csv = f"{Keyword}_Jobs.csv"
-    res = JobsScrapping(Staff_url, Hr_url, Data_csv, Keyword)
+    res = JobsScrapping(Staff_url, Jobdinfer_url, Hr_url, Data_csv, Keyword)
     res.csv_file_open()
